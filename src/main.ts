8b6a360df73db7f23f707ff6c7996e5347b9e704
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-04-15 07:37:14 -0400

    psuedo mainline

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // Find workflow files in the repository:
+    // Map each to the real workflow directory:
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // Find mapped workflows that no longer exist:
+    // Delete each:
   } catch (error) {
     core.setFailed(error.message)
   }
